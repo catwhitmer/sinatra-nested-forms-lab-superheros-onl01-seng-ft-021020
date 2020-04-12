73d2c283ca82c7202652f5987d4faeb3857fc9ea
Cathrin Whitmer <catwhitmer@gmail.com>
2020-04-12 23:49:28 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,23 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
-    set :views, Proc.new { File.join(root, "../views/") }
+  set :views, Proc.new { File.join(root, "../views/") }
 
+  get '/' do
+    erb :super_hero
+  end
+  
+  get '/teams' do 
+    @heroes = Hero.all
+    erb :team
+  end
+  
 
+  post '/teams' do
+    @team = Team.new(params[:team][:name], params[:team][:motto])
+       
+    @hero = Hero.new(params[:name], params[:power], params[:bio])
+
+    erb :team
+  end
 end
